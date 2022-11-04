d4064205b8fd7ce216440671e4dd85be67d9975f
David Gallay <david.gallay@heig-vd.ch>
2022-11-04 12:05:13 +0100

    to test

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use rayon::{prelude::*, iter::plumbing::UnindexedConsumer};
+use anyhow::{anyhow, Result};
 use std::mem;
 
 fn n_to_vec(n: usize, base: usize) -> Option<Vec<usize>> {
@@ -64,21 +65,51 @@ impl Base {
 
 pub struct BruteForce {
     current: Vec<usize>,
-    charset_size: usize,
+    size: usize,
 }
 
 impl BruteForce {
-    fn new(charset_size: usize) -> BruteForce {
+    pub fn new(size: usize) -> BruteForce {
         BruteForce {
             current: Vec::new(),
-            charset_size: charset_size,
+            size,
         }
+    }
+    pub fn chunks(mut self, count: usize, size: usize) -> Result<Vec<BruteForceChunk>> {
+        if count < 2 {
+            return Err(anyhow!("Count must be greater than 2"));
+        }
+        if size < 2 {
+            return Err(anyhow!("Size must be greater than 2"));
+        }
+        let size = size - 1;
+        let to_add = n_to_vec(size, self.size).unwrap();
+        let mut vec = Vec::new();
+        vec.reserve(count);
+        for _ in 0..count {
+            vec.push(
+                BruteForceChunk {
+                    bf: BruteForce {
+                        current: self.next().unwrap(),
+                        size: self.size
+                    },
+                    size,
+                    count,
+                    index: 0
+                }
+            );
+            add_vec(&mut self.current, &to_add, self.size);
+        }
+        Ok(vec)
+    }
+    pub fn with_charset(&self, charset: &str) -> String {
+        convert_vec(&self.current, &charset)
     }
 }
 impl Iterator for BruteForce {
     type Item = Vec<usize>;
 
-    fn next(&mut self) -> Option<Self::Item> {
+     fn next(&mut self) -> Option<Self::Item> {
         let mut i = 0;
         loop {
             if self.current.len() == i {
@@ -87,7 +118,7 @@ impl Iterator for BruteForce {
             }
             let x = self.current[i]
                 .checked_add(1)
-                .map(|x| x % self.charset_size)
+                .map(|x| x % self.size)
                 .unwrap_or(0);
             self.current[i] = x;
             if x != 0 {
@@ -100,14 +131,71 @@ impl Iterator for BruteForce {
 
     // Documentation: override nth instead of "skip"
     fn nth(&mut self, n: usize) -> Option<Self::Item> {
-        let to_add = n_to_vec(n, self.charset_size).unwrap();
-        add_vec(&mut self.current, &to_add, self.charset_size);
+        let to_add = n_to_vec(n, self.size).unwrap();
+        add_vec(&mut self.current, &to_add, self.size);
         Some(self.current.clone())
     }
     /* Experimental
     fn advance_by(&mut self, n: usize) -> Result<(), usize> {
     }*/
 }
+
+pub struct BruteForceChunk {
+    bf: BruteForce,
+    size: usize,
+    count: usize, // Number of chunks
+    index: usize,
+}
+
+impl BruteForceChunk {
+    pub fn with_charset(&self, charset: &str) -> String {
+        self.bf.with_charset(charset)
+    }
+}
+
+impl Clone for BruteForce {
+    fn clone(&self) -> BruteForce {
+        BruteForce { current: self.current.clone(), size: self.size }
+    }
+}
+impl Clone for BruteForceChunk {
+    fn clone(&self) -> BruteForceChunk {
+        BruteForceChunk {
+            bf: self.bf.clone(),
+            size: self.size,
+            count: self.count,
+            index: 0
+        }
+    }
+}
+
+impl Iterator for BruteForceChunk {
+    type Item = Vec<usize>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index < self.size {
+            self.index += 1;
+        } else {
+            self.index = 0;
+            // We need to increment `count` time the value
+            let to_add = n_to_vec(self.size, self.size).unwrap();
+            for _ in 0..self.count {
+                add_vec(&mut self.bf.current, &to_add, self.bf.size);
+            }
+
+        }
+        self.bf.next()
+    }
+
+    // Documentation: override nth instead of "skip"
+    /*fn nth(&mut self, n: usize) -> Option<Self::Item> {
+        let to_add = n_to_vec(self.size, self.size).unwrap();
+        add_vec(&mut self.current, &to_add, self.size);
+        Some(self.current.clone())
+    }*/
+}
+
+
 /*
 pub trait ParallelIterator: Sized + Send {
     type Item: Send;
@@ -132,7 +220,7 @@ pub struct StringBruteForce {
     charset: String,
 }
 
-fn convert_vec(vec: &Vec<usize>, charset: &str) -> String {
+pub fn convert_vec(vec: &Vec<usize>, charset: &str) -> String {
     String::from_iter(vec.iter().filter_map(|i| charset.chars().nth(*i as usize)))
 }
 
@@ -141,7 +229,7 @@ impl StringBruteForce {
         StringBruteForce {
             brute_force: BruteForce {
                 current: Vec::new(),
-                charset_size: charset.len(),
+                size: charset.len(),
             },
             charset: charset.to_string(),
         }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
 mod lib;
+use brute_forcing::{BruteForce, convert_vec};
 use lib::StringBruteForce;
+use rayon::prelude::*;
 
 fn main() {
-    for text in StringBruteForce::new("abcdefghijklmnopqrstuvwxyz")
+    /*for text in StringBruteForce::new("abcdefghijklmnopqrstuvwxyz")
         .skip(1_000_000)
         .take(1_000_000)
     {
         println!("{}", text);
+    }*/
+    let CHUNK_SIZE = 500usize;
+    let charset = "abcdefghijklmnopqrstuvwxyz";
+    let charset_size = charset.chars().count();
+    let res = BruteForce::new(charset_size).chunks(5, CHUNK_SIZE);
+    if let Err(e) = res {
+        eprintln!("{}", e);
+        return;
     }
+    let mut chunks = res.unwrap();
+    for (i, c) in chunks.iter_mut().enumerate() {
+        let tmp = c.with_charset(charset);
+        // let value = convert_vec(&tmp, charset);
+        println!("Chunk {}: {:?}", i, tmp);
+    }
+    /*let chunk =  chunks[0].clone();
+    for c in chunk.take(CHUNK_SIZE + 2) {
+        let value = convert_vec(&c, charset);
+        println!("{:?}", value);
+    }*/
+
+    chunks.par_iter_mut().enumerate().for_each(|(i, c)| {
+        c.for_each(|value| {
+            println!("Chunk {}: {}", i, convert_vec(&value, charset))
+        })
+    })
 }
